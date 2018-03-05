7eabe6d50d5b68cb6ceb06eb8c24f63a46612915
GabrielVelarde <gabto8@gmail.com>
2018-03-05 00:40:35 -0500

    Se modificó la interfaz de usuario y se agrego contenido representativo de la organización.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,27 @@
 class PagesController < ApplicationController
+
+  #Back end for pages/index
   def index
   end
 
+  #Back end for pages/home
   def home
   end
-
+  #Back end for pages/servicios
   def serv
   end
-
+  #Back end for pages/nosotros
   def nosot
   end
 
+  #Back end for pages/nosotros 
   def contact
   end
+
+  def entrev
+  end
+
+  def creclut
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  get '/home' => 'pages#home'
+ # get '/home' => 'pages#home'
 
-  get '/serv' => 'pages#serv'
+  #get '/serv' => 'pages#serv'
 
   get '/nosot' => 'pages#nosot'
 
   get '/contact' => 'pages#contact'
+
+  get '/reclut' => 'pages#reclut'
+
+  get '/entrev' => 'pages#entrev'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
